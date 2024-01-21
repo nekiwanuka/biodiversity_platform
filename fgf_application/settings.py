5812e40d93e35a6cf3b209291676df896655c3be
nekiwanuka <nekiwanuka@gmail.com>
2024-01-21 05:41:37 +0300

    Add Auth

--- fgf_application/settings.py
+++ fgf_application/settings.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import environ
-
+from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -163,15 +163,14 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'info@fgf.com'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'fgfsytems@gmail.com'
-EMAIL_HOST_PASSWORD = 'ehadmponenmyvgcf'
-
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
 # Django-allauth settings
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
